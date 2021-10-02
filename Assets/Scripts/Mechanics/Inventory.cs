b94dff94284445f333dfd5c89e9ef512c61f2ed4
Jakub Szewczyk <jakubszewczyk999@gmail.com>
2021-10-02 12:25:00 +0200

    Added picking up items

--- Assets/Scripts/Mechanics/Inventory.cs
+++ Assets/Scripts/Mechanics/Inventory.cs
@@ -1,21 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-    void Update()
-    {
-        
-    }
+    Item pickedUpItem = null;
+    public Image miniature;
 
     public void PickUpItem(Item item)
     {
-
-
+        if (!HasItem())
+        {
+            pickedUpItem = item;
+            item.gameObject.SetActive(false);
+            miniature.color = new Color(1, 1, 1, 1);
+            miniature.sprite = item.inventoryMiniature;
+        }
+    }
+    public void PlaceItem(Vector3 position)
+    {
+        if (HasItem())
+        {
+            pickedUpItem.transform.position = position;
+            pickedUpItem.gameObject.SetActive(true);
+            pickedUpItem = null;
+            miniature.color = new Color(1, 1, 1, 0);
+            miniature.sprite = null;
+        }
+    }
+    public bool HasItem()
+    {
+        return pickedUpItem;
     }
 }
--- Assets/Scripts/Mechanics/ItemPicker.cs
+++ Assets/Scripts/Mechanics/ItemPicker.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 [RequireComponent(typeof(Inventory))]
 public class ItemPicker : MonoBehaviour
 {
-    Inventory inventory;
+    public Transform source;
+    private Inventory inventory;
     RaycastHit hit;
 
     void Start()
     {
         inventory = GetComponent<Inventory>();
-        
     }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            TryPickUp();
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+            PlaceItem();
     }
-    void FixedUpdate()
+    void TryPickUp()
     {
-        
+        if(Raycast())
+        {
+            GameObject hitObject = hit.collider.gameObject;
+            if (hitObject.CompareTag("Item"))
+            {
+                inventory.PickUpItem(hitObject.GetComponent<Item>());
+            }
+        }
+    }
+    void PlaceItem()
+    {
+        if(Raycast())
+        {
+            Vector3 placePos = hit.point;
+            inventory.PlaceItem(placePos);
+        }
+    }
+
+    bool Raycast()
+    {
+        return source && Physics.Raycast(new Ray(source.position, source.forward), out hit, 10);
     }
 }
